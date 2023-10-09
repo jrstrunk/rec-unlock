11ad51f15634ab8eeb2f414d4466e46f5e321226
John Strunk <developer@jrstrunk.com>
2023-10-09 15:47:49 -0400

    feat: implement SimpleResourceUnlock smart contract

---
+++ src/SimpleResourceUnlock.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.21;
+
+contract SimpleResourceUnlock {
+    mapping(string => string) private _resourceVals;
+    mapping(string => uint256) public resourceCosts;
+    mapping(string => address) public resourceOwners;
+    string[] public resourceDescriptions;
+
+    error Unauthorized();
+    error InsufficientFunds();
+
+    function setResource(string memory key, string memory value, uint256 cost) public {
+        if(resourceOwners[key] != address(0) && resourceOwners[key] != msg.sender) {
+            revert Unauthorized();
+        }
+        _resourceVals[key] = value;
+        resourceCosts[key] = cost;
+        resourceOwners[key] = msg.sender;
+        resourceDescriptions.push(key);
+    }
+
+    function getResourceDescriptions() public view returns (string[] memory) {
+        return resourceDescriptions;
+    }
+
+    function getCost(string memory key) public view returns (uint256) {
+        return resourceCosts[key];
+    }
+
+    function getResource(string memory key) public payable returns (string memory) {
+        if (msg.value < resourceCosts[key]) {
+            revert InsufficientFunds();
+        }
+        return _resourceVals[key];
+    }
+
+}
---
+++ test/SimpleResourceUnlock.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.21;
+
+import {Test, console2} from "forge-std/Test.sol";
+import {SimpleResourceUnlock} from "../src/SimpleResourceUnlock.sol";
+
+contract CounterTest is Test {
+    SimpleResourceUnlock public recUnlock;
+
+    function setUp() public {
+        recUnlock = new SimpleResourceUnlock();
+    }
+
+    function test_SetResource() public {
+        uint256 cost = 1;
+        string memory key = "testKey";
+        recUnlock.setResource(key, "test", cost);
+        assertEq(recUnlock.getCost(key), cost);
+    }
+}
