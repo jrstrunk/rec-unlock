4b58a480886a768b2ec5cb79c6362f7b39f490b4
John Strunk <developer@jrstrunk.com>
2023-10-11 08:42:36 -0400

    test: add tests to get a resource value and to revert when payment is too little

--- test/SimpleResourceUnlock.t.sol
+++ test/SimpleResourceUnlock.t.sol
@@ -7,15 +7,34 @@ import {SimpleResourceUnlock} from "../src/SimpleResourceUnlock.sol";
 contract CounterTest is Test {
     SimpleResourceUnlock public recUnlock;
 
+    error InsufficientFunds();
+
     function setUp() public {
         recUnlock = new SimpleResourceUnlock();
     }
 
     function test_GetResourceCost() public {
-        uint256 cost = 1;
+        uint256 cost = 10;
         string memory key = "testKey";
         recUnlock.setResource(key, "test", cost);
         assertEq(recUnlock.getCost(key), cost);
     }
 
+    function test_GetResource() public {
+        uint256 cost = 10;
+        string memory key = "testKey";
+        string memory rec = "test";
+        recUnlock.setResource(key, rec, cost);
+
+        string memory result = recUnlock.getResource{value: cost}(key);
+        assertEq(result, rec);
+    }
+
+    function test_RevertWhen_PayResourceCostTooLittle() public {
+        uint256 cost = 10;
+        string memory key = "testKey";
+        recUnlock.setResource(key, "test", cost);
+        vm.expectRevert(InsufficientFunds.selector);
+        recUnlock.getResource{value: cost - 1}(key);
+    }
 }
